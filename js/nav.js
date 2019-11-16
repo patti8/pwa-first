c7017b0c3116269b4569cd0f901a2c7fdd275a8a
rvld <pattipeilohyterry@gmail.com>
2019-11-16 16:32:52 +0800

    adding event listener for menu click

--- js/nav.js
+++ js/nav.js
@@ -14,34 +14,47 @@ document.addEventListener("DOMContentLoaded", function() {
         document.querySelectorAll(".topnav, .sidenav").forEach(function(elm) {
           elm.innerHTML = xhttp.responseText;
         });
+
+        // registered event listener for every menu link
+        document.querySelectorAll(".sidenav, .topnav").forEach(function(elm) {
+          elm.addEventListener("click", function(event) {
+            // closed sidenav
+            var sidenav = document.querySelector(".sidenav");
+            M.Sidenav.getInstance(sidenav).close();
+
+            // load page content a called
+            page = event.target.getAttribute("href").substr(1);
+            loadPage(page);
+          });
+        });
       }
     };
     xhttp.open("GET", "nav.html", true);
     xhttp.send();
   }
 
-  // load content
-  var page = window.location.hash.substr(1);
-  if (page == "") page = "home";
-  loadPage(page);
+  // Load page content
+var page = window.location.hash.substr(1);
+if (page == "") page = "home";
+loadPage(page);
 
-  function loadPage(page) {
-    var xhttp = new XHTMLHttpRequest();
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4) {
-        var content = document.querySelector('#body-content');
-        if (this.status == 200 ) {
-          content.innerHTML = xhttp.responseText
-        } else if (this.status == 400) {
-          content.innerHTML = "<p>Halaman tidak ditemukan.</p>";
-        } else {
-          content.innerHTML = "<p>Ups.. halaman tidak dapat diakses.</p>";
-        }
+function loadPage(page) {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4) {
+      var content = document.querySelector("#body-content");
+      if (this.status == 200) {
+        content.innerHTML = xhttp.responseText;
+      } else if (this.status == 404) {
+        content.innerHTML = "<p>Halaman tidak ditemukan.</p>";
+      } else {
+        content.innerHTML = "<p>Ups.. halaman tidak dapat diakses.</p>";
       }
-    };
-    xhttp.open("GET", "pages/" + page + ".html", true);
-    xhttp.send();
-  }
+    }
+  };
+  xhttp.open("GET", "pages/" + page + ".html", true);
+  xhttp.send();
+}
 
 
 });
